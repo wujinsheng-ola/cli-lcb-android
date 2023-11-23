87eae6d3096deb26a2a0fc160d257f7f4fe71be7
hulk <wu.jinsheng@olaola.chat>
2023-11-23 17:54:11 +0800

    update

---
+++ subset/common/src/main/java/com/salton123/base/ViewBindingActivity.kt
@@ -0,0 +1,42 @@
+package com.salton123.base
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.viewbinding.ViewBinding
+import com.salton123.base.feature.ImmersionFeature
+import com.salton123.common.R
+import kt.singleClick
+
+/**
+ * Time:2023/11/23 16:12
+ * Author:
+ * Description:
+ */
+abstract class ViewBindingActivity<VB : ViewBinding> : DelegateActivity() {
+    lateinit var viewBind: VB
+    abstract fun getViewBinding(): VB
+
+    override fun getContentView(): View {
+        viewBind = lazy { getViewBinding() }.value
+        return viewBind.root
+    }
+
+
+    override fun initVariable(savedInstanceState: Bundle?) {
+        addFeature(ImmersionFeature(this))
+    }
+
+    override fun getLayout(): Int = 0
+    open var tvBack: TextView? = null
+    open var tvMore: TextView? = null
+    open var tvTitle: TextView? = null
+    override fun getTitleBar(): View {
+        val titleView = layoutInflater.inflate(R.layout.common_title_bar, null)
+        tvTitle = titleView.findViewById(R.id.tvTitle)
+        tvMore = titleView.findViewById(R.id.tvMore)
+        tvBack = titleView.findViewById(R.id.tvBack)
+        tvBack?.singleClick { finish() }
+        return titleView
+    }
+}
--- subset/user-profile/src/main/java/sg/partying/lcb/profile/ui/ContactUsActivity.kt
+++ subset/user-profile/src/main/java/sg/partying/lcb/profile/ui/ContactUsActivity.kt
@@ -1,8 +1,8 @@
 package sg.partying.lcb.profile.ui
 
-import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.salton123.base.BaseViewBindingActivity
+import com.salton123.base.ViewBindingActivity
+import com.salton123.profile.R
 import com.salton123.profile.databinding.ActivityContactUsBinding
 import com.salton123.soulove.Constants
 import kt.singleClick
@@ -14,14 +14,12 @@ import kt.toast
  * Description:
  */
 @Route(path = Constants.Router.Profile.CONTACT_US)
-class ContactUsActivity : BaseViewBindingActivity<ActivityContactUsBinding>() {
+class ContactUsActivity : ViewBindingActivity<ActivityContactUsBinding>() {
     override fun getViewBinding() = ActivityContactUsBinding.inflate(layoutInflater)
-    override fun initVariable(savedInstanceState: Bundle?) {
-    }
-
     override fun initViewAndData() {
         viewBind.apply {
             ivIcon.singleClick { toast("hello") }
         }
+        tvTitle?.setText(R.string.contact_us)
     }
 }
