a32817b6e107cb02fd5bda00a06a2928cffda84b
hulk <wu.jinsheng@olaola.chat>
2023-11-23 15:14:52 +0800

    update

--- subset/user-profile/src/main/java/sg/partying/lcb/profile/ui/AccountSettingActivity.kt
+++ subset/user-profile/src/main/java/sg/partying/lcb/profile/ui/AccountSettingActivity.kt
@@ -1,8 +1,8 @@
-package sg.partying.lcb.android.ui
+package sg.partying.lcb.profile.ui
 
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.salton123.profile.R
 import com.salton123.soulove.Constants
-import sg.partying.lcb.android.R
 import sg.partying.lcb.base.BaseTitleActivity
 
 /**
@@ -14,5 +14,9 @@ import sg.partying.lcb.base.BaseTitleActivity
 class AccountSettingActivity : BaseTitleActivity() {
     override fun getLayoutId(): Int = R.layout.activity_contact_us
 
+    override fun initViewAndData() {
+        super.initViewAndData()
+        tvTitle.setText(R.string.account_setting)
+    }
 
 }
--- subset/user-profile/src/main/java/sg/partying/lcb/profile/ui/ContactUsActivity.kt
+++ subset/user-profile/src/main/java/sg/partying/lcb/profile/ui/ContactUsActivity.kt
@@ -1,8 +1,8 @@
-package sg.partying.lcb.android.ui
+package sg.partying.lcb.profile.ui
 
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.salton123.profile.R
 import com.salton123.soulove.Constants
-import sg.partying.lcb.android.R
 import sg.partying.lcb.base.BaseTitleActivity
 
 /**
@@ -15,4 +15,8 @@ class ContactUsActivity : BaseTitleActivity() {
     override fun getLayoutId(): Int = R.layout.activity_contact_us
 
 
+    override fun initViewAndData() {
+        super.initViewAndData()
+        tvTitle.setText(R.string.contact_us)
+    }
 }
